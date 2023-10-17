7938fe5fae5ee85b046a2944b0e47e7389c758da
OriginalBinaryHustler <mr.watkin@live.co.uk>
2023-10-17 14:27:40 +0100

    complete

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -12,7 +12,9 @@ function Navbar() {
     <nav className="navbar navbar-expand-sm navbar-light bg-light">
       
         <Link to="/" className="navbar-brand">
-          <img src="logo.svg" alt="Logo" className="navbar-logo" />
+        <img src="https://www.reshot.com/preview-assets/icons/TZCXQGV5F4/maps-TZCXQGV5F4.svg" alt="logo" className="img-fluid" />
+        <h4 className="text-center text-bold">MyPlace</h4>
+          
         </Link>
         <button className="navbar-toggler" type="button" onClick={toggleMenu}>
           <span className="navbar-toggler-icon"></span>
@@ -20,7 +22,7 @@ function Navbar() {
         <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`}>
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link to="/" className="nav-link">Home</Link>
+              <Link to="/" className="nav-link text-primary"><i><strong>Home</strong></i></Link>
             </li>
             <li className="nav-item">
               <Link to="/login" className="nav-link">Log In</Link>
