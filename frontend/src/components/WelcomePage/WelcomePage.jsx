f12ba90269d4d221e330e48c7e05c9a504f1c3e6
alalina <alalinaerm@gmail.com>
2023-10-22 14:19:43 +0100

    changed css for welcome page and navbar

--- frontend/src/components/WelcomePage/WelcomePage.jsx
+++ frontend/src/components/WelcomePage/WelcomePage.jsx
@@ -1,27 +1,20 @@
-import "./WelcomePage.css";
 import Navbar from "../Navbar/Navbar";
 
 function WelcomePage() {
   return (
     <>
-      <main id="welcome-page" className="bg-main">
+      <div className="welcome-page__container">
         <Navbar />
-        <div className="welcome-page__container">
-          <div className="welcome-page__message text-image">
-            <h1 className="welcome-page__heading">
-              Beautiful Places of the world
-            </h1>
-            <p>Look back at memories and plan for the next ones</p>
-          </div>
-          <div className="welcome-page__map">
-            <img
-              src="/welcome-map.jpeg"
-              alt="welcome-map"
-              className="welcome-map"
-            />
-          </div>
+        <div className="welcome-page__text">
+          <h1 className="welcome-page__heading">
+            Beautiful Places of the world
+          </h1>
+          <p className="welcome-page__subtitle">Look back at memories and plan for the next ones</p>
         </div>
-      </main>
+        <footer>
+          <p className="welcome-page__copyright">Copyright © by MyPlace team: Alina Ermakova, Rikie Patrick, Roberto Quadraccia, Claudia Alves, Yasien Watkin </p>
+        </footer>
+      </div>
     </>
   );
 }
