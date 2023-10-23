e2bf2528bf06f3302e1b538736570327c1c3bd11
alalina <alalinaerm@gmail.com>
2023-10-23 16:54:10 +0100

    update location info and close after saving

--- frontend/src/components/mapView/MarkerDetails.jsx
+++ frontend/src/components/mapView/MarkerDetails.jsx
@@ -18,11 +18,13 @@ const MarkerDetails = ({ details, setDetails }) => {
   useEffect(() => {
     if (details) {
       setIsFavourite(details.favourite);
+      setIsVisited(details.visited)
     }
   }, [details]);
 
   const closeDetails = () => {
     setDetails(null);
+    setIsEdit(!isEdit)
   };
 
   if (details) {
@@ -51,6 +53,7 @@ const MarkerDetails = ({ details, setDetails }) => {
         .updateCity(update, details._id)
         .then((data) => {
           setDetails(data.city);
+          closeDetails()
         })
         .catch((error) => {
           console.log(error);
@@ -73,13 +76,14 @@ const MarkerDetails = ({ details, setDetails }) => {
       setIsVisited(!isVisited);
     };
 
-    const handleRatingChange = (e) => {
-      setNewRating(e.target.value);
-    };
-
     return (
       <section id="marker-details">
         <div className="marker-details__options">
+          <img
+            className="marker-details__heart"
+            src={edit}
+            onClick={toggleEdit}
+          />
           <img
             className="marker-details__heart"
             alt="favourite"
@@ -89,35 +93,33 @@ const MarkerDetails = ({ details, setDetails }) => {
           />
         </div>
         <div className="marker-details__user">
-          <img
-            src={`${
-              user.profileImage
+          <img src={user.profileImage}/>
+          {/* <img
+            src={`${user.profileImage
                 ? user.profileImage
                 : "https://api.dicebear.com/7.x/avataaars/svg"
-            }`}
+              }`}
             alt="avatar"
-          />
-          <p className="bold">{details.user.username}</p>
+          /> */}
+          <p>{details.user.username}</p>
         </div>
         <div className="marker-details__place">
-          <h2 className="marker-details__name">{details.name}</h2>
-          <h2 className="marker-details__rating">
-            {[...Array(rating).keys()].map((_) => {
-              return <StarIcon />;
-            })}
-          </h2>
+          <p className="marker-details__name">{details.name}</p>
+          {!isEdit && (
+            <h2 className="marker-details__rating">
+              {[...Array(rating).keys()].map((_) => {
+                return <StarIcon />;
+              })}
+            </h2>
+          )}
         </div>
-        <img
-          className="marker-details__heart"
-          src={edit}
-          onClick={toggleEdit}
-        />
         {isEdit ? (
           <>
-            <div className="checkbox__container">
+            <div className="checkbox__container--edit">
               <label className="checkbox__label">
                 <input
                   type="checkbox"
+                  value={visited}
                   checked={isVisited}
                   onClick={toggleVisitedChange}
                 />
@@ -138,17 +140,17 @@ const MarkerDetails = ({ details, setDetails }) => {
             </div>
           </>
         ) : (
-          <>
-            <h3>{vistedDate}</h3>
-            <p>{memory}</p>
+          <div className="marker-details__container">
+            <p>Visited: {vistedDate}</p>
+            <p className="marker-details__memory">{memory}</p>
             <img
               className="marker-details__photo"
               src="https://images.unsplash.com/photo-1683009686716-ac2096a5a73b?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             />
-          </>
+          </div>
         )}
         {/* <div className="marker-details--scroll"> */}
-          {/* <div className="checkbox__container">
+        {/* <div className="checkbox__container">
             <label className="checkbox__label">
               <input
                 type="checkbox"
@@ -160,7 +162,7 @@ const MarkerDetails = ({ details, setDetails }) => {
             <p>Visited</p>
           </div> */}
 
-          {/* {isVisited && (
+        {/* {isVisited && (
             <>
               <StarRating setRating={setRating} value={rating} />
               <div className="form__input-box">
