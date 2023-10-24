ae6092af58ace84d36579e2168844e8029768663
Roberto <roberto.quadraccia90@gmail.com>
2023-10-24 16:05:23 +0100

    User's and friends' names displaying correctly

--- frontend/src/components/mapView/MapView.jsx
+++ frontend/src/components/mapView/MapView.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useContext } from "react";
+import { CurrentUserContext } from "../../context/CurrentUserContext";
 import { useNavigate } from "react-router-dom";
 import Map, {
   Marker,
@@ -45,6 +46,8 @@ const MapView = ({ cityPins, setCityPins, friend }) => {
   const searchRef = useRef();
   // const search = new MapboxSearchBox();
   const markerDetailsPopupRef = useRef();
+
+  const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
 
   useEffect(() => {
     authApi
@@ -122,7 +125,10 @@ const MapView = ({ cityPins, setCityPins, friend }) => {
 
   return (
     <>
+    { !friend ?
+    <div>{currentUser.username}</div> :
     <div>{`${friend}'s map`}</div>
+    }
       <Map
         ref={myMap}
         id="myMap"
