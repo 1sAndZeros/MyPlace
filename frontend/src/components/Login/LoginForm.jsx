45b85c6a5ce366f54381ef021702f2ab2e747f17
1sAndZeros <rikie_p@hotmail.com>
2023-12-29 15:59:07 +0000

    Update package.json and README.md, add loading / message on submit

--- frontend/src/components/Login/LoginForm.jsx
+++ frontend/src/components/Login/LoginForm.jsx
@@ -1,21 +1,31 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import SlideShow from "../SlideShow/SlideShow";
-import EyeOpen from "../../assets/icons/eye-open.svg?react";
-import EyeClosed from "../../assets/icons/eye-closed.svg?react";
-import errorImg from "../../assets/error.svg";
-import errorClose from "../../assets/Close_square.svg";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SlideShow from '../SlideShow/SlideShow';
+import EyeOpen from '../../assets/icons/eye-open.svg?react';
+import EyeClosed from '../../assets/icons/eye-closed.svg?react';
+import errorImg from '../../assets/error.svg';
+import errorClose from '../../assets/Close_square.svg';
+import Loading from '../../assets/loading.svg?react';
+import DelayedMessage from '../DelayedMessage';
+import { useEffect } from 'react';
 
 const LoginForm = ({
   onLogIn,
   authError,
   setAuthError,
-  handleCloseError
+  handleCloseError,
+  submitting,
+  setSubmitting,
 }) => {
-  
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
   const [showPassword, setShowPassword] = React.useState(false);
+
+  useEffect(() => {
+    return () => {
+      setSubmitting(false);
+    };
+  }, [setSubmitting]);
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -37,73 +47,81 @@ const LoginForm = ({
       password,
     });
 
-    setEmail("");
-    setPassword("");
-    setAuthError("");
+    setEmail('');
+    setPassword('');
+    setAuthError('');
   };
 
   return (
     <>
-      <div className="container">
-        <div className="container-panel container-panel_left">
+      <div className='container'>
+        <div className='container-panel container-panel_left'>
           <SlideShow />
         </div>
-        <div className="container-panel container-panel_right">
-          <form className="form" onSubmit={handleSubmit}>
-            <h2 className="form__title">Log In</h2>
-            <div className="form__input-box">
-              <label className="form__label">Email</label>
+        <div className='container-panel container-panel_right'>
+          <form className='form' onSubmit={handleSubmit}>
+            <h2 className='form__title'>Log In</h2>
+            <div className='form__input-box'>
+              <label className='form__label'>Email</label>
               <input
-                type="text"
-                className="form__input"
-                id="email"
+                type='text'
+                className='form__input'
+                id='email'
                 value={email}
                 onChange={handleEmailChange}
               />
             </div>
-            <div className="form__input-box">
-              <div className="form__password__label">
-                <label className="form__label">Password</label>
+            <div className='form__input-box'>
+              <div className='form__password__label'>
+                <label className='form__label'>Password</label>
                 <div
-                  className="form__password__wrapper"
+                  className='form__password__wrapper'
                   onClick={handleShowPassword}
-                  type="button"
+                  type='button'
                 >
                   {showPassword ? <EyeOpen /> : <EyeClosed />}
-                  <p>{showPassword ? "hide" : "show"}</p>
+                  <p>{showPassword ? 'hide' : 'show'}</p>
                 </div>
               </div>
               <input
-                type={showPassword ? "text" : "password"}
-                className="form__input"
-                id="password"
+                type={showPassword ? 'text' : 'password'}
+                className='form__input'
+                id='password'
                 value={password}
                 onChange={handlePasswordChange}
               />
             </div>
-            <button className="form__button" id="submit" type="submit">
-              Log In
+            <button
+              className='form__button'
+              disabled={submitting}
+              id='submit'
+              type='submit'
+            >
+              {!submitting ? 'Log In' : <Loading />}
             </button>
-            <p className="form__link">
-              Don`t have an account? Please,{" "}
-              <Link to="/signup" className="form__link">
+            {submitting && (
+              <DelayedMessage message='Please wait. It can take up to 1 minute to start the server.' />
+            )}
+            <p className='form__link'>
+              Don`t have an account? Please,{' '}
+              <Link to='/signup' className='form__link'>
                 Sign up
               </Link>
             </p>
           </form>
           {authError ? (
-            <div className="error-auth">
-              <div className="error-auth__box">
+            <div className='error-auth'>
+              <div className='error-auth__box'>
                 <img
-                  className="error-auth__icon"
+                  className='error-auth__icon'
                   src={errorImg}
-                  alt="error icon"
+                  alt='error icon'
                 />
-                <p className="error-auth__message">{authError}</p>
+                <p className='error-auth__message'>{authError}</p>
                 <img
-                  className="error-auth__icon error-auth__icon--close"
+                  className='error-auth__icon error-auth__icon--close'
                   src={errorClose}
-                  alt="error close"
+                  alt='error close'
                   onClick={handleCloseError}
                 />
               </div>
@@ -114,6 +132,6 @@ const LoginForm = ({
       {/* <Navbar/> */}
     </>
   );
-}
+};
 
 export default LoginForm;
